56c0cd4ec9c010e6cb1f07c748ec0f6d18c89d48
MadJiGit <raykovmladen@gmail.com>
2021-04-07 16:55:42 +0300

    fixing error into AppLoading props onFinish={() => setFontsLoaded({isReady: true })} or onFinish={() => setFontsLoaded(true)}

--- App.js
+++ App.js
@@ -10,22 +10,20 @@ const getFonts = () => Font.loadAsync({
 
 export default function App() {
   // loadAsync is not work correctly
-	// const [fontsLoaded, setFontsLoaded] = useState(false);
+	const [fontsLoaded, setFontsLoaded] = useState(false);
 
-  // if(fontsLoaded){
-  //   return (
-  //     <Home />
-  //   );
-  // } else {
-  //   return ( 
-  //     <AppLoading 
-  //       startAsync={getFonts}
-  //       onFinish={() => setFontsLoaded( {isReady: true })}
-  //     />
-  //   );
-  // }
-
-  return (
-    <Home />
-  );
+  if(fontsLoaded){
+    return (
+      <Home />
+    );
+  } else {
+    return ( 
+      <AppLoading 
+        startAsync={getFonts}
+        // May be onFinish={() => setFontsLoaded(true)}
+        onFinish={() => setFontsLoaded({isReady: true })}
+        onError={console.warn}
+      />
+    );
+  }
 } 
--- screens/home.jsx
+++ screens/home.jsx
@@ -5,7 +5,7 @@ import { StyleSheet, View, Text } from 'react-native';
 export default function Home() {
     return (
         <View style={styles.container}>
-            <Text>
+            <Text style={styles.titletext}>
                 Home Screen
             </Text>
         </View>
@@ -16,5 +16,10 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         padding: 24
+    },
+    titletext: {
+        fontFamily: 'nunito-bold',
+        fontSize: 18,
     }
+
 });
